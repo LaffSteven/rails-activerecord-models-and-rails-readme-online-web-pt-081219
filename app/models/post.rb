8135b06c66dacb6a2c02817afcc43525071f4450
Steven Laff <steve.laff@gmail.com>
2020-01-07 22:41:15 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
+<<<<<<< HEAD
   
   def post_summary
     self.title + " - " + self.description
   end
     
+=======
+>>>>>>> 4ce8c7fe522d85a2ee2579b13ca130e2e02ef184
 end
